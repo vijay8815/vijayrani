6a6cd6e223be2da4a901939ec61896c6e766051e
vijay8815 <vm786995@gmail.com>
2024-08-13 14:35:31 +0530

    thirdb commit

--- src/components/Hooks.js
+++ src/components/Hooks.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 
 const Hooks = () => {
     const [count,setCount] = useState(1);
-    function decrement(){
+    function crement(){
         setCount(count+5);
     }
   return (
     <>
         <p>{count}</p>
-        <button onClick={()=>decrement()} >Click Me</button>
+        <button onClick={()=>crement()} >Click Me</button>
     </>
   )
 }
